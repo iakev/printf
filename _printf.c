0923a494882d748273549fc5df79650596a29ae7
iakev <kirimikmwongera@gmail.com>
2022-08-08 21:36:01 +0000

    Added functions to the printf one for dealing with a newline character and the main _printf function

--- _printf.c
+++ _printf.c
@@ -40,3 +40,70 @@ void _print_s(va_list s, unsigned int *count)
 	}
 }
 
+/**
+ * _print_n - Prints a newline character
+ * @c: The backslash
+ * @d: The 'n' in the backslash
+ * @i: Pointer to the while loop iterator in printf
+ */
+void _print_n(char c, char d, unsigned int *i)
+{
+	if (c == '\\')
+	{
+		if (d == 'n')
+		{
+			_putchar('\n');
+			(*i) += 2;
+		}
+	}
+}
+
+/**
+ * _printf - main function to print according to a format
+ * @format: character strings composed of 0 or more directives
+ *		and format specifiers
+ * Return: Number of characters printed excluding the null byte
+ */
+int _printf(const char *format, ...)
+{
+	unsigned int i, j, count;
+	va_list ap;
+	prnt_t prnts[] = {
+		{"c", _print_c},
+		{"s", _print_s},
+		{NULL, NULL}
+	};
+
+	va_start(ap, format);
+	i = 0;
+	count = 0;
+	while (format && format[i] != '\0')
+	{
+		_print_n(format[i], format[i + 1], &i);
+		if (format[i] != '%' || (format[i] == '%' && format[i + 1] == '%'))
+		{
+			_putchar(format[i]);
+			count++;
+			i++;
+		}
+		else
+		{
+			if (format[i] == '%')
+			{
+				j = 0;
+				while (prnts[j].c != NULL)
+				{
+					if (format[i + 1] == *prnts[j].c)
+					{
+						prnts[j]._puts(ap, &count);
+						break;
+					}
+					j++;
+				}
+				i += 2;
+			}
+		}
+	}
+	va_end(ap);
+	return (count);
+}
