4f60ce257943acadc57e954207c160c0a8d11383
Ahlam Abdulkhalek <alhamny@gmail.com>
2022-08-09 20:48:24 +0300

    Corrected main.h and added some bit of interger specifier

--- main.h
+++ main.h
@@ -20,28 +20,7 @@ void _print_s(va_list, unsigned int *);
 void _print_c(va_list, unsigned int *);
 int _printf(const char *format, ...);
 void _print_n(char c, char d, unsigned int *i);
-#endif
-#ifndef MAIN_H
-#define MAIN_H
-#include <stdarg.h>
-#include <stdlib.h>
-int _putchar(char c);
-
-/**
- * struct prnt - flag / function object
- * @c: flag
- * @_puts: function pointer
- */
-
-typedef struct prnt
-{
-	char *c;
-	void (*_puts)(va_list, unsigned int *);
-} prnt_t;
-
-void _print_s(va_list, unsigned int *);
-void _print_c(va_list, unsigned int *);
-int _printf(const char *format, ...);
-void _print_n(char c, char d, unsigned int *i);
 void check_specifiers(va_list, char, char, unsigned int *, unsigned int *);
+void _print_d(va_list d, unsigned int *count);
+
 #endif
